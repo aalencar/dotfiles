601ae0cf22a547008d5646e4505e21f719991a0f
Alexandre Alencar <alencardev@gmail.com>
2016-12-18 13:54:09 -0200

    Linking script ask the user if he really wants to run it

--- symlink.sh
+++ symlink.sh
@@ -1,15 +1,24 @@
 #/bin/bash
 
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+function link {
+	BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# vim
-ln -sfv ${BASEDIR}/vim/vimrc ~/.vimrc
+	# vim
+	ln -sfv ${BASEDIR}/vim/vimrc ~/.vimrc
 
-# tmux
-ln -sfv ${BASEDIR}/tmux/tmux.conf ~/.tmux.conf
-ln -sFhv ${BASEDIR}/tmux/tmux.sessions/ ~/.tmux-sessions
+	# tmux
+	ln -sfv ${BASEDIR}/tmux/tmux.conf ~/.tmux.conf
+	ln -sFhv ${BASEDIR}/tmux/tmux.sessions/ ~/.tmux-sessions
 
-# scripts
-ln -sFhv ${BASEDIR}/bin/ ~/bin
+	# scripts
+	ln -sFhv ${BASEDIR}/bin/ ~/bin
+}
 
-
+while true; do
+	read -p "The script may override some files on the Home directory. Continue? (Yy/Nn)" answer
+	case $answer in
+		[Yy]* ) link; break;;
+		[Nn]* ) exit;;
+		* ) echo "Please answer yes or no.";;
+	esac
+done
